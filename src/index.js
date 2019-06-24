6cb7b5463ca4bf36c7666f1eb3198f6cc1e9a15f
Sander  Smit <>
2019-06-24 13:01:30 +0200

    first commit from sourcetree

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+//loading class from the node modules folder -> "react" class
 import React from 'react';
+//loading class from the node modules folder -> "react-dom" class
 import ReactDOM from 'react-dom';
+//loading index.css from the the root folder "src"
 import './index.css';
+//loading App class out of the App.js file from the the root folder "src" 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+//targetting the Main class "App.js" with the Id "root" (see index.html)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
